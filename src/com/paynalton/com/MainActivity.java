2030b8ece585f6ff2ff7025492daa10f5725503e
paynalton <cxescalona@gmail.com>
2014-05-19 13:24:20 -0500

    programación Lista

--- src/com/paynalton/com/MainActivity.java
+++ src/com/paynalton/com/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import java.text.*;
+import java.util.Formatter;
 
 public class MainActivity extends Activity
 {
+	private EditText _fuView;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -14,4 +17,12 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+	
+	public void calcular(View view){
+		_fuView=(EditText)findViewById(R.id.fondouterino);
+		float fu=Float.parseFloat(_fuView.getText().toString());
+		float resultado=(fu*8)/7;
+		String resf=getResources().getString(R.string.sdg);
+		((TextView)findViewById(R.id.resultado)).setText(String.format(resf,resultado));
+	}
 }
